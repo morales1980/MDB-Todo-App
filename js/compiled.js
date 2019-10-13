78dd7b3683f98990b0a12b658df172e9c3d60def
Darek.Szotowicz <dariusz.szotowicz@protonmail.com>
2019-10-13 21:34:57 +0200

    add edit todo functionality

--- js/compiled.js
+++ js/compiled.js
@@ -1,35 +1,85 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
+  var display = document.querySelector('#display');
   var buttonAdd = document.querySelector('#button-add');
   var buttonRemove = document.querySelector('#button-remove');
   var buttonClear = document.querySelector('#button-clear');
-  var todoDisplay = document.querySelector('#display');
-  var todoCounter = 0;
+  var editForm = document.querySelector('#edit-form');
+  var editMessage = document.querySelector('#edit-message');
+  var elementToEdit;
+  var editButtons;
+  var todoCounter = 0; //============================== edit todo
+
+  var grabTodoEditButtons = function grabTodoEditButtons() {
+    editButtons = document.querySelectorAll('.todo__item-edit-button');
+    editButtons.forEach(function (editButton) {
+      editButton.addEventListener('click', editButtonClickHandler);
+    });
+  };
+
+  var editButtonClickHandler = function editButtonClickHandler(e) {
+    editForm.classList.toggle('show');
+    elementToEdit = e.target.previousElementSibling;
+  };
+
+  var setNewTodoContent = function setNewTodoContent(element, content) {
+    element.textContent = content;
+  };
+
+  var formSubmitHandler = function formSubmitHandler(e) {
+    e.preventDefault();
+    var newTodoContent = e.target[0].value; //text newTodoContent against Non-blank/non-whitespace string
+
+    var regex = /^(?!\s*$).+/;
+
+    if (regex.test(newTodoContent)) {
+      setNewTodoContent(elementToEdit, newTodoContent);
+      editForm.classList.toggle('show');
+      editMessage.classList.toggle('show');
+    } else {
+      editMessage.classList.toggle('show');
+    }
+
+    e.target[0].value = '';
+  };
+
+  editForm.addEventListener('submit', formSubmitHandler); //============================== add todo
 
   var addTodo = function addTodo() {
+    var div = document.createElement('div');
     var paragraph = document.createElement('p');
+    var button = document.createElement('button');
+    div.classList.add('todo__item');
+    paragraph.classList.add('todo__item-label');
+    button.classList.add('todo__item-edit-button');
     paragraph.textContent = "Task number ".concat(todoCounter + 1);
-    todoDisplay.appendChild(paragraph);
+    button.textContent = 'Edit';
+    div.appendChild(paragraph);
+    div.appendChild(button);
+    display.appendChild(div);
+    grabTodoEditButtons();
     todoCounter++;
-  }; // I'm aware that ChildNode.remove() method is available. Hence .remove() is not supported in IE and regardless I could polyfill the remove() method in Internet Explorer 9, I've decided to use removeChild() instead.
+  };
 
+  buttonAdd.addEventListener('click', addTodo); //============================== remove todo
+  // I'm aware that ChildNode.remove() method is available. Hence .remove() is not supported in IE and regardless I could polyfill the remove() method in Internet Explorer 9, I've decided to use removeChild() instead.
 
   var removeTodo = function removeTodo() {
-    if (todoDisplay.lastElementChild) {
-      todoDisplay.removeChild(todoDisplay.lastElementChild);
+    if (display.lastElementChild) {
+      display.removeChild(display.lastElementChild);
       todoCounter--;
     }
   };
+
+  buttonRemove.addEventListener('click', removeTodo); //============================== clear todo
 
   var clearTodo = function clearTodo() {
-    while (todoDisplay.lastElementChild) {
-      todoDisplay.removeChild(todoDisplay.lastElementChild);
+    while (display.lastElementChild) {
+      display.removeChild(display.lastElementChild);
       todoCounter--;
     }
   };
 
-  buttonAdd.addEventListener('click', addTodo);
-  buttonRemove.addEventListener('click', removeTodo);
   buttonClear.addEventListener('click', clearTodo);
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,88 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const display = document.querySelector('#display');
   const buttonAdd = document.querySelector('#button-add');
   const buttonRemove = document.querySelector('#button-remove');
   const buttonClear = document.querySelector('#button-clear');
-  const todoDisplay = document.querySelector('#display');
+  const editForm = document.querySelector('#edit-form');
+  const editMessage = document.querySelector('#edit-message');
+
+  let elementToEdit;
+  let editButtons;
   let todoCounter = 0;
 
+  //============================== edit todo
+  const grabTodoEditButtons = () => {
+    editButtons = document.querySelectorAll('.todo__item-edit-button');
+    editButtons.forEach((editButton) => {
+      editButton.addEventListener('click', editButtonClickHandler);
+    });
+  }
+
+  const editButtonClickHandler = (e) => {
+    editForm.classList.toggle('show');
+    elementToEdit = e.target.previousElementSibling;
+  }
+
+  const setNewTodoContent = (element, content) => {
+    element.textContent = (content);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    const newTodoContent = e.target[0].value;
+
+    //text newTodoContent against Non-blank/non-whitespace string
+    const regex = /^(?!\s*$).+/;
+    if(regex.test(newTodoContent)) {
+      setNewTodoContent(elementToEdit, newTodoContent);
+      editForm.classList.toggle('show');
+      editMessage.classList.toggle('show');
+    } else {
+      editMessage.classList.toggle('show');
+    }
+    e.target[0].value = '';
+  };
+
+  editForm.addEventListener('submit', formSubmitHandler);
+
+  //============================== add todo
   const addTodo = () => {
+    const div = document.createElement('div');
     const paragraph = document.createElement('p');
+    const button = document.createElement('button');
+
+    div.classList.add('todo__item');
+    paragraph.classList.add('todo__item-label');
+    button.classList.add('todo__item-edit-button');
+
     paragraph.textContent = (`Task number ${todoCounter + 1}`);
-    todoDisplay.appendChild(paragraph);
+    button.textContent = ('Edit');
+
+    div.appendChild(paragraph);
+    div.appendChild(button);
+    display.appendChild(div);
+
+    grabTodoEditButtons();
     todoCounter++;
   };
+  buttonAdd.addEventListener('click', addTodo);
 
+  //============================== remove todo
   // I'm aware that ChildNode.remove() method is available. Hence .remove() is not supported in IE and regardless I could polyfill the remove() method in Internet Explorer 9, I've decided to use removeChild() instead.
   const removeTodo = () => {
-    if(todoDisplay.lastElementChild) {
-      todoDisplay.removeChild(todoDisplay.lastElementChild);
+    if(display.lastElementChild) {
+      display.removeChild(display.lastElementChild);
       todoCounter--;
     }
   };
-
-  const clearTodo = () => {
-    while(todoDisplay.lastElementChild) {
-      todoDisplay.removeChild(todoDisplay.lastElementChild);
-      todoCounter--;
-    }
-  };
-
-  buttonAdd.addEventListener('click', addTodo);
   buttonRemove.addEventListener('click', removeTodo);
+
+  //============================== clear todo
+  const clearTodo = () => {
+    while(display.lastElementChild) {
+      display.removeChild(display.lastElementChild);
+      todoCounter--;
+    }
+  };
   buttonClear.addEventListener('click', clearTodo);
 });
